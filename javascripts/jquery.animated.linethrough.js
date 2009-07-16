e9ecd5f50c9c3649558c055d80ee47f2d4dfcd24
Zach Inglis <zach@lt3media.com>
2009-07-16 19:26:18 +0100

    Once completed, the inner divs are removed and it's replaced in it's parents contents.

--- javascripts/jquery.animated.linethrough.js
+++ javascripts/jquery.animated.linethrough.js
@@ -1,11 +1,11 @@
 jQuery(document).ready(function () {
-  jQuery('#line').animateStrikethrough();
-  
+  jQuery('#line').animateStrikethrough(40);
+
   // Debug code
   jQuery('#debug').text("Javascript loaded");
 });
 
-jQuery.fn.animateStrikethrough = function(speed, callback){
+jQuery.fn.animateStrikethrough = function(speed){
   return this.each(function(){
     if(jQuery(this).find('.strike').length == 0) {
       jQuery(this).wrapInner("<span class='letters-left'></span>");
@@ -19,7 +19,7 @@ jQuery.fn.animateStrikethrough = function(speed, callback){
     setInterval(function () {
       jQuery(line).strikeThroughFirstLetter();
       
-    }, 40)
+    }, speed);
   });
 };
 
@@ -27,9 +27,14 @@ jQuery.fn.strikeThroughFirstLetter = function(){
   return this.each(function(){
     jQuery(this).find('.strike').transposeFirstLetter(jQuery(this).find('.letters-left'));
     jQuery(this).find('.letters-left').removeFirstLetter();
+    
+    if(jQuery(this).find('.letters-left').length != 0 && jQuery(this).find('.letters-left').text().length == 0) {
+      jQuery(this).addClass('strike');
+      jQuery(this).text(jQuery(this).find('.strike').text());
+      jQuery(this).find('.strike, .letters-left').remove();
+    }
   });
 };
-
 
 jQuery.fn.transposeFirstLetter = function(toTransposeTo){
   return this.each(function(){
