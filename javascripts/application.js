729fe24c78228ec45573719075cd91afcef7cf99
Zach Inglis <zach@lt3media.com>
2009-07-17 04:31:16 +0100

    Removed the needless .remove() call

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function () {  
   jQuery('#line').animateStrikethrough(40, function () {
-    jQuery('#debug').append("<p>Completed</p>");
+    jQuery('#debug').append("<p><strong>Completed!</strong></p>");
   });
 
   // Debug code
--- javascripts/jquery.animated.linethrough.js
+++ javascripts/jquery.animated.linethrough.js
@@ -5,15 +5,17 @@ jQuery.fn.animateStrikethrough = function(speed, callback){
       jQuery(this).prepend("<span class='strike'></span>");
     }
     
-    var line    = jQuery(this)
+    var line    = jQuery(this);
     var strike  = jQuery(line).find('.strike');
     var left    = jQuery(line).find('.letters-left');
 
     strikethrough = setInterval(function () {
       jQuery(line).strikeThroughFirstLetter();
-      if(jQuery(this).find('.letters-left').length == 0 && typeof callback == "function") {
-        callback();
+      if(jQuery(left).text().length == 0) {
+        jQuery(line).addClass('strike');
+        jQuery(line).text(jQuery(line).find('.strike').text());
         clearInterval(strikethrough);
+        if(typeof callback == "function") callback();
       }
     }, speed);
   });
@@ -23,13 +25,6 @@ jQuery.fn.strikeThroughFirstLetter = function(){
   return this.each(function(){
     jQuery(this).find('.strike').transposeFirstLetter(jQuery(this).find('.letters-left'));
     jQuery(this).find('.letters-left').removeFirstLetter();
-    
-    if(jQuery(this).find('.letters-left').length != 0 && jQuery(this).find('.letters-left').text().length == 0) {
-      jQuery(this).addClass('strike');
-      jQuery(this).text(jQuery(this).find('.strike').text());
-      jQuery(this).find('.strike, .letters-left').remove();
-      strikethrough_finished = true;
-    }
   });
 };
 
